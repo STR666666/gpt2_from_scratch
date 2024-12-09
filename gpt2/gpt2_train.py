e3612be1467a7352d49041ee8255edf053f8a341
Scott R <renscott66@gmail.com>
2024-12-09 14:02:06 -0500

    B=16, T=1024 -> train

--- gpt2/gpt2_train.py
+++ gpt2/gpt2_train.py
@@ -1,4 +1,5 @@
 import math
+import time
 import tiktoken
 import torch
 import torch.nn as nn
@@ -207,7 +208,7 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
 # Data Loader
-train_loader = DataLoaderLite(4, 32)
+train_loader = DataLoaderLite(16, 1024)
 
 # model configuration
 model = GPT(GPTConfig())
@@ -216,13 +217,18 @@ model.to(device)
 # optimization
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x,y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1-t0) / 1000
+    tokens_per_sec = (train_loader.B * train_loader.T) / dt
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tokens/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
